88d8f77c46bb86cbe1f2ca96e5908a1b5747b4b9
paulz <senecalpaul@outlook.fr>
2025-02-12 21:13:48 +0100

    header

---
+++ header/src/app.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header style={{ background: "#f0f0f0", padding: "1rem", textAlign: "center" }}>
+      <h1>Header Micro Frontend</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ header/webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 3001,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "header",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Header": "./src/App",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
